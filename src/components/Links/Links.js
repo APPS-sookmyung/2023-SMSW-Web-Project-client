93672bcf7cc8e23330d5083c7a563a88574ffad2
rudalstj <110973127+mxinseo@users.noreply.github.com>
2023-07-27 17:36:50 +0900

    홈화면의 링크 요소 구현

---
+++ src/components/Links/Links.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Links.css";
+
+const Links = () => {
+  return (
+    <div className="links">
+      <a className="smlinks" href="https://smsso.sookmyung.ac.kr/">
+        Portal
+      </a>
+      <a className="smlinks" href="https://snowboard.sookmyung.ac.kr/">
+        Snowboard
+      </a>
+      <a className="smlinks" href="https://snowe.sookmyung.ac.kr/">
+        Snowe
+      </a>
+      <a className="smlinks" href="https://snoway.sookmyung.ac.kr/">
+        Snoway
+      </a>
+    </div>
+  );
+};
+
+export default Links;
