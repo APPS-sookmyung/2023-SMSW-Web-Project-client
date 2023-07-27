429e709702577fb2c5db668df27f6f68c6c2e570
rudalstj <110973127+mxinseo@users.noreply.github.com>
2023-07-27 17:36:23 +0900

    푸터 구현

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="location">
+        <span>
+          (04310) 서울특별시 용산구 청파로47길 100 (청파동2가) 숙명여자대학교
+          소프트웨어학부
+        </span>
+        <span>
+          학부사무실 : 명신관 425호 | TEL. 02)710-9431 | FAX. 02)710-9296 |
+          공동기기실, 취업관리 TEL. 02)710-9295
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
