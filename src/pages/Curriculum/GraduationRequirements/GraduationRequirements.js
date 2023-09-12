e2295fd5aa76bc95fdf54805e2464a163c4bedf1
mxinseo <jade201913@gmail.com>
2023-09-12 20:20:01 +0900

    졸업요건 내용 구현

--- src/pages/Curriculum/GraduationRequirements/GraduationRequirements.js
+++ src/pages/Curriculum/GraduationRequirements/GraduationRequirements.js
@@ -1,11 +1,25 @@
 import React from "react";
+import DSGraduationRequirements from "../GraduationRequirements/DSGraduationRequirements";
+import CSGraduationRequirements from "../GraduationRequirements/CSGraduationRequirements";
+import SCGraduationRequirements from "../GraduationRequirements/SCGraduationRequirements";
+import "./GraduationRequirements.css";
 
 const GraduationRequirements = () => {
   return (
-    <div>
-      <div>this is GraduationRequirements</div>
-      <div>this is GraduationRequirements</div>
-      <div>this is GraduationRequirements</div>
+    <div className="GraduationRequirementsPart">
+      <div className="GraduationRequirementsTable">
+        <p>컴퓨터과학전공</p>
+        <CSGraduationRequirements />
+      </div>
+      <div className="GraduationRequirementsTable">
+        <p>소프트웨어융합전공</p>
+        <SCGraduationRequirements />
+      </div>
+      <div className="GraduationRequirementsTable">
+        <p>데이터사이언스전공</p>
+
+        <DSGraduationRequirements />
+      </div>
     </div>
   );
 };
