ff66d39420ea1bd1eb4cc135a4ad6448d9f99a12
mxinseo <jade201913@gmail.com>
2023-09-12 21:11:21 +0900

    학회 내용 구현

--- src/pages/StudentActivity/Club/Club.js
+++ src/pages/StudentActivity/Club/Club.js
@@ -1,11 +1,38 @@
 import React from "react";
+import ClubList from "./ClubList.js";
+import "./Club.css";
 
 const Club = () => {
+  const ViewClubList = ({ club }) => {
+    return (
+      <div>
+        <table className="clubTable">
+          <tbody>
+            <tr className="clubTableRow">
+              <td>
+                <div className="clubName">{club.name}</div>
+                <div className="clubDetail">{club.detail}</div>
+                <div className="clubLeadingProfessor">
+                  지도교수: {club.leadingProfessor}
+                </div>
+                <div className="clubSite">사이트: {club.site}</div>
+                <div className="clubLocation">위치: {club.location}</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <div>this is Club</div>
-      <div>this is Club</div>
-      <div>this is Club</div>
+    <div className="ClubPart">
+      <p className="ClubTitle">소프트웨어학부 동아리</p>
+      <div>
+        {ClubList.map((club) => (
+          <ViewClubList club={club} key={club.name} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/StudentActivity/Club/ClubList.js
@@ -0,0 +1,52 @@
+export const ClubList = [
+  {
+    name: "SOLUX",
+    detail:
+      "소프트웨어학부의 리눅스 학회에서 출발한 SOLUX는 이제 더 다양한 분야에서의 도전을 시작합니다. 웹, 아두이노, 빅데이터 등의 분야와  자신의 전공을 융합하고 다양한 분야에 관심있는 학우들이 모여 IT 프로젝트와 세미나를 진행합니다. 더 나은 자신을 위한 특별한 동아리, 우리는 솔룩스입니다.",
+    leadingProfessor: "소프트웨어학부 문봉희 교수",
+    site: "http://solux.sookmyung.ac.kr/",
+    location: "학생회관 504호",
+  },
+  {
+    name: "SISS",
+    detail:
+      "SISS ( Sookmyung Information Security Study ) 숙명 정보 보안 동아리로서, 이제는 사회적 문화의 하나로 자리잡은 컴퓨터와 그 컴퓨터들간의 network 사이의 방대한 정보의 흐름, 정보가치의 보안에 대해 사회적 중요성을 인식하고 2001년 12월 15일 설립되었습니다. SISS는 정보 보안을 공부하는 모임으로, 네트워크, 시스템, 어플리케이션보안 및 암호학 등의 보안 분야를 공부합니다. 2006년부터 한국정보보호진흥원(KISA)에서 주관한 대학 정보보호 동아리 지원사업에 지원사업 대상 동아리로 선정되어온 활동성 있는 동아리입니다.",
+    leadingProfessor: "소프트웨어학부 이광수 교수",
+    site: "http://siss.sookmyung.ac.kr/",
+    location: "-",
+  },
+  {
+    name: "FORZA",
+    detail:
+      "Forza는 숙명여대 컴퓨터과학부 프로그래밍 동아리입니다. 관심분야가 비슷한 선후배와 함께 전공과 관련된 스터디를 하거나, 프로젝트를 하고 있습니다. 또한 1:1 멘토/멘티제를 취하고 있어서, 전공에 대한 선배들의 조언을 얻을 수 있는 동아리입니다.",
+    leadingProfessor: "소프트웨어학부 김주균 교수",
+    site: "-",
+    location: "-",
+  },
+  {
+    name: "ALGOS",
+    detail:
+      "Algos는 숙명여자대학교 ACM-ICPC 프로그램 경진대회 준비 동아리 입니다. CM-ICPC란 국제 컴퓨터학회 ACM(Association for Computing Machinery)에서 주최하고 IBM이 후원하는 국제 대학생 프로그래밍 경진대회 (International Collegiate Programming Contest)로서 누가 더 빠른 시간 안에 더 많은 문제에 대한 정확한 프로그램을 작성 했는가로 순위가 결정되는 세계적 권위의 대학생 프로그래밍 대회입니다. 알고스는 매년 가을에 개최되는 아시아지역예선 대회 참가를 목적으로 다양한 알고리즘, 자료구조에 대한 스터디 및 문제풀이와 프로그램 작성 훈련을 정기적으로 진행하고 있습니다.",
+    leadingProfessor: "소프트웨어학부 창병모 교수",
+    site: "http://cafe.naver.com/smalgos",
+    location: "-",
+  },
+  {
+    name: "APPS",
+    detail:
+      "APPS는 숙명여자대학교 모바일 컴퓨팅 동아리입니다. 스마트폰을 비롯한 모바일 컴퓨팅 환경에서 요구되는 소프트웨어, 사용자인터페이스, 알고리즘 등을 공부하는 동아리로, 주요 활동 내용은 모바일앱 설계, 개발을 중심으로 공모전, 연구과제를 준비하는 것이며, 기초가 되는 자료구조, 자바프로그래밍, 모바일 프로그래밍 등의 스터디를 진행합니다. 그밖에 최신 IT기술 동향을 공부하고, 지도교수님, 선후배들 간의 활발한 교류를 통하여 전문 SW 엔지니어의 소양을 갖추는 것을 목적으로 하고 있습니다.",
+    leadingProfessor: "소프트웨어학부 유석종 교수",
+    site: "http://cafe.naver.com/sookmyungapps.cafe",
+    location: "-",
+  },
+  {
+    name: "DACOS",
+    detail:
+      "DACOS(Data Analysis Club Of Sookmyung)는 숙명여자대학교 유일 데이터 분석 학부동아리입니다. 데이터 분석 스터디 진행 및 활동 공유와 더불어 학생들이 프로젝트를 진행 시 공모전 참가, 논문 작성 및 특허 등록과 같은 확실한 결과물을 창출하여 의미 있는 활동을 하는 동아리입니다. 또한 동아리 활동의 활성화를 통해 숙명여대 교내 학생들의 네트워크 형성과 오프라인 중심의 커뮤니케이션의 장이 되기를 기대하고 있습니다.",
+    leadingProfessor: "소프트웨어학부 이기용 교수",
+    site: "-",
+    location: "-",
+  },
+];
+
+export default ClubList;
