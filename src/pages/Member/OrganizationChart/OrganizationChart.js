a0cd0774e6d49933ca12788246cda98f70090ff4
mxinseo <jade201913@gmail.com>
2023-09-30 23:05:59 +0900

    교수 소개 와 조직도 페이지 헤더 통일

--- src/pages/Member/OrganizationChart/OrganizationChart.js
+++ src/pages/Member/OrganizationChart/OrganizationChart.js
@@ -4,14 +4,18 @@ import OrganizationChartImg from "../../../assets/OrganizationChart.jpeg";
 
 const OrganizationChart = () => {
   return (
-    <div className="OrganizationChartPart">
-      <p className="OrganizationChartTitle">소프트웨어학부 조직도</p>
-      <div className="OrganizationChartImg">
-        <img
-          style={{ width: "800px", margin: "35px" }}
-          src={OrganizationChartImg}
-          alt="조직도"
-        ></img>
+    <div>
+      <div className="OrganizationChartHeaderImg">
+        <p className="OrganizationChartTitle">소프트웨어학부 조직도</p>
+      </div>
+      <div className="OrganizationChartPart">
+        <div className="OrganizationChartImg">
+          <img
+            style={{ width: "800px", margin: "35px" }}
+            src={OrganizationChartImg}
+            alt="조직도"
+          ></img>
+        </div>
       </div>
     </div>
   );
