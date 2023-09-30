b06ad0b624d0a7c26fe2858b0ee27fd01a965a2a
mxinseo <jade201913@gmail.com>
2023-09-30 22:55:34 +0900

    교수 소개 페이지 헤더 css 변경
    배경 이미지 밝은 이미지로 수정
    폰트 크기 키움

--- src/pages/Member/OrganizationChart/OrganizationChart.js
+++ src/pages/Member/OrganizationChart/OrganizationChart.js
@@ -6,11 +6,13 @@ const OrganizationChart = () => {
   return (
     <div className="OrganizationChartPart">
       <p className="OrganizationChartTitle">소프트웨어학부 조직도</p>
-      <img
-        className="OrganizationChartImg"
-        src={OrganizationChartImg}
-        alt="조직도"
-      ></img>
+      <div className="OrganizationChartImg">
+        <img
+          style={{ width: "800px", margin: "35px" }}
+          src={OrganizationChartImg}
+          alt="조직도"
+        ></img>
+      </div>
     </div>
   );
 };
