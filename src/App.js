9464d1f4b0eec91ecbdf4982f7c0f4eaa06dc1a9
mxinseo <jade201913@gmail.com>
2023-11-04 00:58:42 +0900

    [Feat] PageLayout -> App.js 이동

--- src/App.js
+++ src/App.js
@@ -8,23 +8,29 @@ import Member from "./pages/Member/Member";
 import StudentActivity from "./pages/StudentActivity/StudentActivity";
 import Home from "./pages/Home/Home";
 
+import PageLayout from "./pages/PageLayout/PageLayout";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<Home />} />
-      <Route path="/Curriculum">
-        <Route path="" element={<Curriculum />} />
-      </Route>
-      <Route path="/MajorInfo">
-        <Route path="" element={<MajorInfo />} />
-      </Route>
-      <Route path="/Member">
-        <Route path="" element={<Member />} />
-      </Route>
-      <Route path="/StudentActivity">
-        <Route path="" element={<StudentActivity />} />
-      </Route>
-    </Routes>
+    <PageLayout header={<Header />} footer={<Footer />}>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/Curriculum">
+          <Route path="" element={<Curriculum />} />
+        </Route>
+        <Route path="/MajorInfo">
+          <Route path="" element={<MajorInfo />} />
+        </Route>
+        <Route path="/Member">
+          <Route path="" element={<Member />} />
+        </Route>
+        <Route path="/StudentActivity">
+          <Route path="" element={<StudentActivity />} />
+        </Route>
+      </Routes>
+    </PageLayout>
   );
 }
 
