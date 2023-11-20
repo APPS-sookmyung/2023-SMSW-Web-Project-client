65596dd94f95c8b4150be9847c63f8ae5837b1c8
mxinseo <jade201913@gmail.com>
2023-11-20 20:58:09 +0900

    Feat: 홈 애니메이션 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,73 +2,86 @@ import React from "react";
 import * as S from "./Home.style.jsx";
 import InfoTitleBox from "../../components/InfoTitleBox/InfoTitleBox.jsx";
 
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+
 const Home = () => {
   return (
     <S.Container>
       <S.MainImg>
         <S.MainInner>
-          <S.MainTitle>Sookmyung</S.MainTitle>
-          <S.MainTitle>Software</S.MainTitle>
-          <S.MainSubTitle>
-            지식정보화 사회를 선도할 소프트웨어 전문 인력을 양성합니다.
-          </S.MainSubTitle>
+          <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true}>
+            <S.MainTitle>Sookmyung</S.MainTitle>
+            <S.MainTitle>Software</S.MainTitle>
+            <S.MainSubTitle>
+              지식정보화 사회를 선도할 소프트웨어 전문 인력을 양성합니다.
+            </S.MainSubTitle>
+          </AnimationOnScroll>
         </S.MainInner>
       </S.MainImg>
 
       <S.Intro>
         <S.IntroInner>
-          <S.IntroTitle>생각하는 힘을 가진 창조적 인재 양성</S.IntroTitle>
-          <S.IntroMajorContainer>
-            <S.IntroMajor>
-              <S.IntroMajorInner>
-                <S.IntroMajorName>컴퓨터과학</S.IntroMajorName>
-              </S.IntroMajorInner>
-            </S.IntroMajor>
-            <S.IntroMajor>
-              <S.IntroMajorInner>
-                <S.IntroMajorName>데이터사이언스</S.IntroMajorName>
-              </S.IntroMajorInner>
-            </S.IntroMajor>
-            <S.IntroMajor>
-              <S.IntroMajorInner>
-                <S.IntroMajorName>소프트웨어융합</S.IntroMajorName>
-              </S.IntroMajorInner>
-            </S.IntroMajor>
-          </S.IntroMajorContainer>
+          <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true}>
+            <S.IntroTitle>생각하는 힘을 가진 창조적 인재 양성</S.IntroTitle>
+          </AnimationOnScroll>
+          <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true}>
+            <S.IntroMajorContainer>
+              <S.IntroMajor>
+                <S.IntroMajorInner>
+                  <S.IntroMajorName>컴퓨터과학</S.IntroMajorName>
+                </S.IntroMajorInner>
+              </S.IntroMajor>
+              <S.IntroMajor>
+                <S.IntroMajorInner>
+                  <S.IntroMajorName>데이터사이언스</S.IntroMajorName>
+                </S.IntroMajorInner>
+              </S.IntroMajor>
+              <S.IntroMajor>
+                <S.IntroMajorInner>
+                  <S.IntroMajorName>소프트웨어융합</S.IntroMajorName>
+                </S.IntroMajorInner>
+              </S.IntroMajor>
+            </S.IntroMajorContainer>
+          </AnimationOnScroll>
         </S.IntroInner>
       </S.Intro>
 
       <S.MajorFieldSection>
         <S.MajorFieldSectionInner>
-          <InfoTitleBox title="전공분야" />
+          <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
+            <InfoTitleBox title="전공분야" />
 
-          <S.MajorField>
-            <S.MajorFieldContainer>
-              <S.MajorFieldTitle>Software Core</S.MajorFieldTitle>
-              <S.MajorFieldDetail>
-                컴퓨터 구조, 운영체제, 프로그래밍 언어
-              </S.MajorFieldDetail>
-              <S.MajorFieldDetail>
-                데이터베이스, 소프트웨어 공학, 인터넷
-              </S.MajorFieldDetail>
-            </S.MajorFieldContainer>
-            <S.MajorFieldContainer>
-              <S.MajorFieldTitle>Application Software</S.MajorFieldTitle>
-              <S.MajorFieldDetail>이차전지 및 전기화학공학</S.MajorFieldDetail>
-              <S.MajorFieldDetail>
-                컴퓨터 그래픽스, 인간-컴퓨터 상호작용
-              </S.MajorFieldDetail>
-            </S.MajorFieldContainer>
-            <S.MajorFieldContainer>
-              <S.MajorFieldTitle>Software Convergence</S.MajorFieldTitle>
-              <S.MajorFieldDetail>
-                빅데이터, 사물인터넷, 디지털컨텐츠, 핀테크
-              </S.MajorFieldDetail>
-              <S.MajorFieldDetail>
-                바이오 융합, 소프트웨어 창업 및 교육
-              </S.MajorFieldDetail>
-            </S.MajorFieldContainer>
-          </S.MajorField>
+            <S.MajorField>
+              <S.MajorFieldContainer>
+                <S.MajorFieldTitle>Software Core</S.MajorFieldTitle>
+                <S.MajorFieldDetail>
+                  컴퓨터 구조, 운영체제, 프로그래밍 언어
+                </S.MajorFieldDetail>
+                <S.MajorFieldDetail>
+                  데이터베이스, 소프트웨어 공학, 인터넷
+                </S.MajorFieldDetail>
+              </S.MajorFieldContainer>
+              <S.MajorFieldContainer>
+                <S.MajorFieldTitle>Application Software</S.MajorFieldTitle>
+                <S.MajorFieldDetail>
+                  이차전지 및 전기화학공학
+                </S.MajorFieldDetail>
+                <S.MajorFieldDetail>
+                  컴퓨터 그래픽스, 인간-컴퓨터 상호작용
+                </S.MajorFieldDetail>
+              </S.MajorFieldContainer>
+              <S.MajorFieldContainer>
+                <S.MajorFieldTitle>Software Convergence</S.MajorFieldTitle>
+                <S.MajorFieldDetail>
+                  빅데이터, 사물인터넷, 디지털컨텐츠, 핀테크
+                </S.MajorFieldDetail>
+                <S.MajorFieldDetail>
+                  바이오 융합, 소프트웨어 창업 및 교육
+                </S.MajorFieldDetail>
+              </S.MajorFieldContainer>
+            </S.MajorField>
+          </AnimationOnScroll>
         </S.MajorFieldSectionInner>
       </S.MajorFieldSection>
 
