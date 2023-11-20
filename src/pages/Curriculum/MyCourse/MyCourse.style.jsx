58fe279e7fe63dcd1aaf03cf43f6b31f13fb6a80
mxinseo <jade201913@gmail.com>
2023-11-20 19:17:52 +0900

    style: margin 수정

--- src/pages/Curriculum/MyCourse/MyCourse.style.jsx
+++ src/pages/Curriculum/MyCourse/MyCourse.style.jsx
@@ -12,7 +12,7 @@ export const SemesterContainer = styled.div`
   flex-direction: column;
   width: 70vw;
   margin: 0 auto;
-  margin-bottom: 2px;
+  margin-bottom: 5px;
 `;
 
 export const SemesterText = styled.h3`
