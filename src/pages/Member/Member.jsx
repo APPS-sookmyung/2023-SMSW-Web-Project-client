82deaa1bb16f1410297fcc02df2e57de61444895
mxinseo <jade201913@gmail.com>
2023-11-20 21:49:47 +0900

    Feat: 구성원 애니메이션 추가

--- src/pages/Member/Member.jsx
+++ src/pages/Member/Member.jsx
@@ -3,13 +3,18 @@ import * as S from "./Member.style.jsx";
 import InfoTitleBox from "../../components/InfoTitleBox/InfoTitleBox.jsx";
 import Professor from "./Professor/Professor.jsx";
 
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+
 const Member = () => {
   return (
     <S.Container>
       <S.MainImg>
         <S.MainInner>
-          <S.MainTitle>교수 소개</S.MainTitle>
-          <S.MainSubTitle>Professor</S.MainSubTitle>
+          <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true}>
+            <S.MainTitle>교수 소개</S.MainTitle>
+            <S.MainSubTitle>Professor</S.MainSubTitle>
+          </AnimationOnScroll>
         </S.MainInner>
       </S.MainImg>
 
@@ -19,11 +24,15 @@ const Member = () => {
 
       <S.OrganizationChartPart>
         <S.InfoTitleBoxContainer>
-          <InfoTitleBox title="조직도" />
+          <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true}>
+            <InfoTitleBox title="조직도" />
+          </AnimationOnScroll>
         </S.InfoTitleBoxContainer>
-        <S.OrganizationChartContainer>
-          <S.OrganizationChart />
-        </S.OrganizationChartContainer>
+        <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true}>
+          <S.OrganizationChartContainer>
+            <S.OrganizationChart />
+          </S.OrganizationChartContainer>
+        </AnimationOnScroll>
       </S.OrganizationChartPart>
     </S.Container>
   );
--- src/pages/Member/Professor/Professor.jsx
+++ src/pages/Member/Professor/Professor.jsx
@@ -3,70 +3,75 @@ import { ProfessorList } from "./ProfessorList.js";
 
 import * as S from "./Professor.style.jsx";
 
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+
 const Professor = () => {
   const ViewProfessorList = ({ professorList }) => {
     return (
-      <S.Container>
-        <S.ProfessorImgContainer>
-          <S.ProfessorImg
-            style={{
-              backgroundImage: `url(${professorList.Image})`,
-            }}
-          >
-            <S.ProfessorName>
-              <S.ProfessorNameFont>{professorList.name}</S.ProfessorNameFont>
-            </S.ProfessorName>
-          </S.ProfessorImg>
-        </S.ProfessorImgContainer>
+      <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true}>
+        <S.Container>
+          <S.ProfessorImgContainer>
+            <S.ProfessorImg
+              style={{
+                backgroundImage: `url(${professorList.Image})`,
+              }}
+            >
+              <S.ProfessorName>
+                <S.ProfessorNameFont>{professorList.name}</S.ProfessorNameFont>
+              </S.ProfessorName>
+            </S.ProfessorImg>
+          </S.ProfessorImgContainer>
 
-        <S.ProfessorDetailOneColumn>
-          <S.Table>
-            <colgroup>
-              <col style={{ width: "30%" }} />
-              <col style={{ width: "70%" }} />
-            </colgroup>
-            <tbody>
-              <tr>
-                <S.Th>직위</S.Th>
-                <S.Td>{professorList.Position}</S.Td>
-              </tr>
-              {professorList.Phone !== "-" && (
+          <S.ProfessorDetailOneColumn>
+            <S.Table>
+              <colgroup>
+                <col style={{ width: "30%" }} />
+                <col style={{ width: "70%" }} />
+              </colgroup>
+              <tbody>
                 <tr>
-                  <S.Th>번호</S.Th>
-                  <S.Td>{professorList.Phone}</S.Td>
+                  <S.Th>직위</S.Th>
+                  <S.Td>{professorList.Position}</S.Td>
                 </tr>
-              )}
-              <tr>
-                <S.Th>이메일</S.Th>
-                <S.Td>{professorList.Email}</S.Td>
-              </tr>
-              <tr>
-                <S.Th>분야</S.Th>
-                <S.Td>{professorList.Field}</S.Td>
-              </tr>
-              {professorList.Location !== "-" && (
+                {professorList.Phone !== "-" && (
+                  <tr>
+                    <S.Th>번호</S.Th>
+                    <S.Td>{professorList.Phone}</S.Td>
+                  </tr>
+                )}
                 <tr>
-                  <S.Th>위치</S.Th>
-                  <S.Td>{professorList.Location}</S.Td>
+                  <S.Th>이메일</S.Th>
+                  <S.Td>{professorList.Email}</S.Td>
                 </tr>
-              )}
-            </tbody>
-          </S.Table>
-          {professorList.Website !== "-" ? (
-            <S.ProfessorSiteBtn href={professorList.Website}>
-              <span
-                style={{
-                  alignSelf: "center",
-                  width: "auto",
-                  height: "auto",
-                }}
-              >
-                🔗
-              </span>
-            </S.ProfessorSiteBtn>
-          ) : null}
-        </S.ProfessorDetailOneColumn>
-      </S.Container>
+                <tr>
+                  <S.Th>분야</S.Th>
+                  <S.Td>{professorList.Field}</S.Td>
+                </tr>
+                {professorList.Location !== "-" && (
+                  <tr>
+                    <S.Th>위치</S.Th>
+                    <S.Td>{professorList.Location}</S.Td>
+                  </tr>
+                )}
+              </tbody>
+            </S.Table>
+            {professorList.Website !== "-" ? (
+              <S.ProfessorSiteBtn href={professorList.Website}>
+                <span
+                  style={{
+                    alignSelf: "center",
+                    width: "auto",
+                    height: "auto",
+                  }}
+                >
+                  🔗
+                </span>
+              </S.ProfessorSiteBtn>
+            ) : null}
+          </S.ProfessorDetailOneColumn>
+        </S.Container>
+      </AnimationOnScroll>
     );
   };
 
